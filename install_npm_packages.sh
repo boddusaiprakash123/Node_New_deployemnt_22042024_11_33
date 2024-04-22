8b668a3c3795fac9f087e203a608a18a053a05e8
boddusaiprakash123 <95060708+boddusaiprakash123@users.noreply.github.com>
2024-04-22 22:25:37 +0530

    Update install_npm_packages.sh

--- install_npm_packages.sh
+++ install_npm_packages.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Install Node.js and npm on Amazon Linux
+sudo yum install -y nodejs npm
 
 # Install PM2 globally
 sudo npm install pm2@latest -g
 
-# Change directory to the Node.js application directory
-cd /home/ec2/my-node-app
+# Change directory to the Node.js application directory if it exists
+if [ -d "/home/ec2/my-node-app" ]; then
+  cd /home/ec2/my-node-app
 
-# Install npm packages with force flag
-npm i --force
+  # Install npm packages with force flag
+  npm i --force
+else
+  echo "Node.js application directory not found."
+fi
