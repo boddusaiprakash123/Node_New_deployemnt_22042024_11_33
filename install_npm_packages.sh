d1c4ec11a980081d360c45c426064565c95f92f2
boddusaiprakash123 <95060708+boddusaiprakash123@users.noreply.github.com>
2024-04-22 21:59:16 +0530

    Update install_npm_packages.sh

--- install_npm_packages.sh
+++ install_npm_packages.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install PM2 globally
+sudo npm install pm2@latest -g
+
 # Change directory to the Node.js application directory
 cd /home/ec2/my-node-app
 
 # Install npm packages with force flag
 npm i --force
-
